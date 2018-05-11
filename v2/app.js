63452b0f57c8cb3473abf42c4f11452a0fefbea3
dudzio <dud2iol@gmail.com>
2018-05-11 19:29:42 +0200

    show route added, show.ejs added

--- v2/app.js
+++ v2/app.js
@@ -10,12 +10,18 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-// Campground.create({name: "Blueberries!", image: "https://cdn.pixabay.com/photo/2018/04/28/16/12/blueberry-3357568_960_720.jpg"},
-//     function(err, campground){
+// Campground.create(
+//     {
+//         name: "Blueberries!",
+//         image: "https://cdn.pixabay.com/photo/2018/04/28/16/12/blueberry-3357568_960_720.jpg",
+//         description: "Yummys Berries!"
+
+//     },function(err, campground){
 //         if(err){
 //             console.log(err);
 //         } else{
@@ -27,22 +33,25 @@ app.get("/", function(req, res){
     res.render("landing");
 });
 
+// INDEX ROUTE
 app.get("/campgrounds", function(req, res){
     // Get all CGs from DB
     Campground.find({}, function(err, allCampgrounds){
         if(err){
             console.log(err);
         }else{
-             res.render("campgrounds", {campgrounds: allCampgrounds});
+             res.render("index", {campgrounds: allCampgrounds});
         }
     });
 });
 
+// CREATE ROUTE
 app.post("/campgrounds", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
-    var newCampground = {name: name, image: image};
-    // Create new CG and save do DB
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // Create new CG and save to DB
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err)
@@ -52,9 +61,21 @@ app.post("/campgrounds", function(req, res){
     });
 });
 
+// NEW ROUTE
 app.get("/campgrounds/new", function(req, res){
     res.render("new");
-})
+});
+
+// SHOW ROUTE
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("show", {campground: foundCampground});
+        }
+    });
+});
 
 app.listen(3000, function(){
 	console.log("The server is running!");
