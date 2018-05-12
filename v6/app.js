9354fa9672783492dd92990cb1155e01cbb5f2ee
dudzio <dud2iol@gmail.com>
2018-05-12 17:45:44 +0200

    auth routes - register section - added

--- v6/app.js
+++ v6/app.js
@@ -11,7 +11,7 @@ var express = require("express"),
 
 
 
-mongoose.connect("mongodb://localhost/yelp_camp_v5");
+mongoose.connect("mongodb://localhost/yelp_camp_v6");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
@@ -107,6 +107,29 @@ app.post("/campgrounds/:id/comments", function(req, res){
                 }
             })
         }
+    });
+});
+
+// ==================================
+// AUTH ROUTES
+// ==================================
+
+// show register form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handle sign up logic
+app.post("/register", function(req, res){
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/campgrounds");
+        })
     });
 });
 
