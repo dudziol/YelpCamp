609d3550709669105fc7473af7ae196b73090070
dudzio <dud2iol@gmail.com>
2018-05-12 17:33:49 +0200

    passport config

--- v6/app.js
+++ v6/app.js
@@ -17,6 +17,18 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
 
+// PASSPORT CONFIG
+app.use(require("express-session")({
+    secret: "Hello there little precious!",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get("/", function(req, res){
     res.render("landing");
 });
