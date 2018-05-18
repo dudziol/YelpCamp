1d36e2070a6f6a148fe536ccb6243eec66752aa0
dudzio <dud2iol@gmail.com>
2018-05-18 09:36:13 +0200

    minor bugs fixed

--- v10/routes/comments.js
+++ v10/routes/comments.js
@@ -32,7 +32,6 @@ router.post("/", isLoggedIn, function(req, res){
                     // save comment
                     campground.comments.push(comment);
                     campground.save();
-                    console.log(comment);
                     res.redirect("/campgrounds/" + campground._id);
                 }
             })
@@ -40,6 +39,34 @@ router.post("/", isLoggedIn, function(req, res){
     });
 });
 
+// COMMENTS EDIT ROUTE
+router.get("/:comment_id/edit", function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            console.log(foundComment);
+            console.log(err);
+            res.redirect("back");
+        }else{
+            res.render("comments/edit", {campground_id: req.params.id, comment: foundComment});
+        }
+    });
+});
+
+// COMMENTS UPDATE ROUTE
+router.put("/:comment_id", function(req, res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
+        if(err){
+            res.redirect("back");
+        }else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
+// COMMENTS DESTROY ROUTE
+router.delete("/:comment_id", function(req, res){
+    res.send("DELETE ROUTE HIT");
+});
 
 // middleware
 function isLoggedIn(req, res, next){
